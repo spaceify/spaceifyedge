2bd3ab23a2a54f21af096a10abd97c8a9c6d4161
Max Vilkki <codise@codise.org>
2016-08-09 18:48:01 +0300

    Updated the install script for other prerequisities and renamed it.

--- data/scripts/install_spaceify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-sudo apt-get install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo sh -c "deb https://apt.dockerproject.org/repo ubuntu-xenial main\ > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update
-sudo apt-get purge lxc-docker
-apt-cache policy docker-engine
-sudo apt-get install linux-image-extra-$(uname -r)
-sudo apt-get update
