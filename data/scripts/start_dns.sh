f467647f26ab29ab560f5cb0faceb770a8ff485d
Petri Savolainen <petri.savolainen@hiit.fi>
2016-08-13 10:01:18 +0300

    Removed spaceifydns

--- data/scripts/start_dns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Called from upstart script /etc/init/spaceifydns.conf to start Spaceify's DNS server.
-# Spaceify Oy 21.4.2015
-
-cd /var/lib/spaceify/code
-node dnsservice >> /var/lib/spaceify/data/logs/spaceifydns.log 2>&1
