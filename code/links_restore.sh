077f68b9f599e50155de7bbd51f14e092432a6ea
jovepsal <jovepsal@cs.helsinki.fi>
2016-11-25 15:02:59 +0200

    Soft links fixed

---
+++ code/links_restore.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+rm binaryrpccommunicator.js > /dev/null 2>&1
+rm callbackbuffer.js > /dev/null 2>&1
+rm logger.js > /dev/null 2>&1
+rm rpccommunicator.js > /dev/null 2>&1
+rm service.js > /dev/null 2>&1
+rm spaceifyapplication.js > /dev/null 2>&1
+rm spaceifyconfig.js > /dev/null 2>&1
+rm spaceifycore.js > /dev/null 2>&1
+rm spaceifyerror.js > /dev/null 2>&1
+rm spaceifynetwork.js > /dev/null 2>&1
+rm spaceifyservice.js > /dev/null 2>&1
+rm spaceifysynchronous.js > /dev/null 2>&1
+rm spaceifyunique.js > /dev/null 2>&1
+rm spaceifyutility.js > /dev/null 2>&1
+rm webrtcclient.js > /dev/null 2>&1
+rm webrtcconnection.js > /dev/null 2>&1
+rm websocketconnection.js > /dev/null 2>&1
+rm websocketrpcconnection.js > /dev/null 2>&1
+rm websocketrpcserver.js > /dev/null 2>&1
+rm websocketserver.js > /dev/null 2>&1
+
+ln -s www/libs/webjsonrpc/binaryrpccommunicator.js binaryrpccommunicator.js > /dev/null 2>&1
+ln -s www/libs/webjsonrpc/callbackbuffer.js callbackbuffer.js > /dev/null 2>&1
+ln -s www/libs/logger.js logger.js > /dev/null 2>&1
+ln -s www/libs/webjsonrpc/rpccommunicator.js rpccommunicator.js > /dev/null 2>&1
+ln -s www/libs/service.js service.js > /dev/null 2>&1
+ln -s www/libs/spaceifyapplication.js spaceifyapplication.js > /dev/null 2>&1
+ln -s www/libs/spaceifyconfig.js spaceifyconfig.js > /dev/null 2>&1
+ln -s www/libs/spaceifycore.js spaceifycore.js > /dev/null 2>&1
+ln -s www/libs/spaceifyerror.js spaceifyerror.js > /dev/null 2>&1
+ln -s www/libs/spaceifynetwork.js spaceifynetwork.js > /dev/null 2>&1
+ln -s www/libs/spaceifyservice.js spaceifyservice.js > /dev/null 2>&1
+ln -s www/libs/spaceifysynchronous.js spaceifysynchronous.js > /dev/null 2>&1
+ln -s www/libs/spaceifyutility.js spaceifyutility.js > /dev/null 2>&1
+ln -s www/libs/spaceifyunique.js spaceifyunique.js > /dev/null 2>&1
+ln -s www/libs/webjsonrpc/webrtcclient.js webrtcclient.js > /dev/null 2>&1
+ln -s www/libs/webjsonrpc/webrtcconnection.js webrtcconnection.js > /dev/null 2>&1
+ln -s www/libs/webjsonrpc/websocketconnection.js websocketconnection.js > /dev/null 2>&1
+ln -s www/libs/webjsonrpc/websocketrpcconnection.js websocketrpcconnection.js > /dev/null 2>&1
+ln -s www/libs/webjsonrpc/websocketrpcserver.js websocketrpcserver.js > /dev/null 2>&1
+ln -s www/libs/webjsonrpc/websocketserver.js websocketserver.js > /dev/null 2>&1
+
